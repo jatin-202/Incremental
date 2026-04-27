f064be6ea486c708989821d9dada2d8d4b8afd21
jatin-202 <dummy@gmail.com>
2026-04-27 19:55:44 +0000

    day-18

--- client/src/app/ipl/components/teamcreate/teamcreate.component.ts
+++ client/src/app/ipl/components/teamcreate/teamcreate.component.ts
@@ -1,4 +1,53 @@
 
-export class TeamCreateComponent {
- 
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-teamcreate',
+  templateUrl: './teamcreate.component.html',
+  styleUrls: ['./teamcreate.component.scss']
+})
+export class TeamCreateComponent implements OnInit {
+
+  teamForm!: FormGroup;
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
+  currentYear = new Date().getFullYear();
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.teamForm = this.fb.group({
+      teamId: [{ value: null, disabled: true }],
+      teamName: ['', Validators.required],
+      location: ['', Validators.required],
+      ownerName: ['', [Validators.required, Validators.minLength(2)]],
+      establishmentYear: [
+        this.currentYear,
+        [Validators.required, Validators.min(1900), Validators.max(this.currentYear)]
+      ]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.teamForm.valid) {
+      this.successMessage = 'Team has been successfully created!';
+      this.errorMessage = null;
+    } else {
+      this.errorMessage = 'Please fill out all required fields correctly.';
+      this.successMessage = null;
+    }
+  }
+
+  resetForm(): void {
+    this.teamForm.reset({
+      teamId: null,
+      teamName: '',
+      location: '',
+      ownerName: '',
+      establishmentYear: this.currentYear
+    });
+    this.successMessage = null;
+    this.errorMessage = null;
+  }
 }
